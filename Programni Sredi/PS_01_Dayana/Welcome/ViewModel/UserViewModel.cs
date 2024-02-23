b6444071a0d1b7da44a28d6cf7add2bbcb60242c
Dayana Uzunova <daqnauzunova@abv.bg>
2024-02-23 19:26:14 +0200

    Add our first exercise.
    Add our first exercise.

---
+++ Programni Sredi/PS_01_Dayana/Welcome/ViewModel/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using Welcome.Others;
+
+namespace Welcome.ViewModel
+{
+    public class UserViewModel
+    {
+        private User _user;
+
+        public UserViewModel(User user)
+        {
+            _user = user;
+        }
+        public string Name
+        {
+            get { return _user.Name; }
+            set { _user.Name = value; }
+        }
+        public string Password
+        {
+            get { return _user.Password; }
+            set { _user.Password = value; }
+        }
+
+        public UserRolesEnum Role
+        {
+            get { return _user.Role; }
+            set { _user.Role = value; }
+        }
+
+    }
+}
