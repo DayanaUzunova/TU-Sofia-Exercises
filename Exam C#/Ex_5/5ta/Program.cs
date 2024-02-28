72ec3d551415b060bd2ff9389647cf40b62d95c0
Dayana Uzunova <daqnauzunova@abv.bg>
2024-02-28 17:41:49 +0200

    Exercises with C#.
    Exercises with C#.

---
+++ Exam C#/Ex_5/5ta/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace _5ta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seaPrice = 680;
+            int mountainPrice = 499;
+
+            int seaCount = int.Parse(Console.ReadLine());
+            int moutainCount = int.Parse(Console.ReadLine());
+
+            decimal profit = 0;
+            string inputLine = "";
+
+            while (true)
+            {
+               inputLine = Console.ReadLine();
+                if (inputLine == "Stop")
+                {
+                       
+                    break;
+                }
+                if (inputLine == "sea" && seaCount != 0)
+                {
+                    profit += seaPrice;
+                    seaCount--;
+                }
+                else if (inputLine == "mountain" && moutainCount != 0)
+                {
+                    profit += mountainPrice;
+                    moutainCount--;
+                }
+             
+                if (seaCount == 0)
+                    if (moutainCount == 0)
+                       
+                break;
+
+                if (moutainCount == 0)
+                    if (seaCount == 0)
+                        
+                break;
+            }
+           if (inputLine!="Stop")
+            {
+                Console.WriteLine("Good job! Everything is sold.");
+                Console.WriteLine($"Profit: {profit} leva.");
+            }
+           else
+            {
+                Console.WriteLine($"Profit: {profit} leva.");
+            }
+        }
+    }
+}
